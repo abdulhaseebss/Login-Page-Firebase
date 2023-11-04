19ff34fb6ee8142ba563b09d30403a2d6e79b86e
abdulhaseebss <m.saleemhaseeb@gmail.com>
2023-11-04 12:16:10 -0700

    Firebase Store

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
-import { getAuth  } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+import { getAuth  } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDfLvdd7IUY49uF63QJylMecwHxicLY78E",
@@ -14,3 +15,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth (app);
+export const db = getFirestore(app);
--- web.js
+++ web.js
@@ -1,39 +1,89 @@
 import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
-import { auth } from "./config.js";
+import { auth, db } from "./config.js";
+import { collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
 
 
-const enjoy = document.querySelector('#enjoy')
 const form = document.querySelector('#form')
 const logOut = document.querySelector('#logOut')
+const title = document.querySelector('#title')
+const description = document.querySelector('#discription')
+const div = document.querySelector('#main-div')
+
+div.style.display = "none"
+
+
+getDataFromFirestore()
 
 onAuthStateChanged(auth, (user) => {
     if (user) {
         const uid = user.uid;
-        console.log(uid);
+        // console.log(uid);
     } else {
         window.location = 'index.html'
     }
 });
 
 
-form.addEventListener('click', (e) => {
+form.addEventListener('submit', async (e) => {
     e.preventDefault()
+    // console.log(title.value);
+    // console.log(description.value);
+
+    div.style.display = "block"
+    try {
+        // console.log(title.value);
+        // console.log(description.value);
+        const docRef = await addDoc(collection(db, "posts"), {
+            title: title.value,
+            description: description.value,
+            uid: auth.currentUser.uid
+        });
+        console.log("Document written with ID: ", docRef.id);
+        getDataFromFirestore()
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    };
+
 })
+const arr = []
+async function getDataFromFirestore() {
+    const querySnapshot = await getDocs(collection(db, "posts"));
+    querySnapshot.forEach((doc) => {
+        arr.push(doc.data());
+    });
+    console.log(arr);
+    arr.map((item) => {
+        div.innerHTML += `
+        <div>
+        <div>
+            <p><span class="h4">Title = </span>${item.title}</p>
+            <p><span class="h4">Description = </span>${item.description}</p>
+        </div><br/><br/>
+    </div>`
+    // render(item)
+    })
+}
 
 logOut.addEventListener('click', () => {
     signOut(auth).then(() => {
         console.log('logout successfully');
-        window.location = 'index.html'
+        window.location = 'login.html'
     }).catch((error) => {
         console.log(error);
     });
 })
 
+// function render(item) {
+//     div.innerHTML = "";
 
-
-
-enjoy.addEventListener('click', () => {
-    enjoy.className = 'animate__animated animate__rubberBand'
-})
-
-
+//     for (let i = 0; i < arr.length; i++) {
+//     //   const item = arr;
+//       div.innerHTML += `
+//         <div>
+//         <div>
+//             <p><span class="h4">Title = </span>${item.title}</p>
+//             <p><span class="h4">Description = </span>${item.description}</p>
+//         </div><br/><br/>
+//     </div>`
+//     }
+//   }
